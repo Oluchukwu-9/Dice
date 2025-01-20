0f379d0c6bda9d1f7dab72b1517dce85ae1d8ff6
oluchukwu <victorogonwa@gmail.com>
2025-01-20 16:58:16 +0100

    completed the task as stated in python crash course

--- dice.py
+++ dice.py
@@ -1,23 +1,15 @@
-"""A simple die model
-"""
+"""A simple die model."""
+
 from random import randint
 
 class Die:
-    """A class that simulates a dice 
-    """
+    """A class that simulates a dice."""
 
-    def __init__(self):
-        """Initialize the attribute of the Die class
-        """        
-        self.sides = 6
+    def __init__(self, sides=6):
+        """Initialize the attribute of the Die class."""
+        self.sides = sides
 
-
-    def roll_die(self, number_of_sides):
-        """Displays a random number between 1 and the number of sides
-
-        Args:
-            number_of_sides (int): The number of sides of a die
-        """
-        self.sides = number_of_sides
+    def roll_die(self):
+        """Display a random number between 1 and the number of sides."""
         chance = randint(1, self.sides)
-        print(chance)
+        return chance
--- main_dice.py
+++ main_dice.py
@@ -1,39 +1,32 @@
-"""A dice simulation
-"""
-from dice import Die
+"""A dice simulation."""
 
-#Make as number of sided die and roll it ten times
+import dice
 
-six_sided = Die()
-six_sided.roll_die(6)
-six_sided.roll_die(6)
-six_sided.roll_die(6)
-six_sided.roll_die(6)
-six_sided.roll_die(6)
-six_sided.roll_die(6)
-six_sided.roll_die(6)
-six_sided.roll_die(6)
-six_sided.roll_die(6)
-six_sided.roll_die(6)
-print("\n")
-six_sided.roll_die(10)
-six_sided.roll_die(10)
-six_sided.roll_die(10)
-six_sided.roll_die(10)
-six_sided.roll_die(10)
-six_sided.roll_die(10)
-six_sided.roll_die(10)
-six_sided.roll_die(10)
-six_sided.roll_die(10)
-six_sided.roll_die(10)
-print("\n")
-six_sided.roll_die(20)
-six_sided.roll_die(20)
-six_sided.roll_die(20)
-six_sided.roll_die(20)
-six_sided.roll_die(20)
-six_sided.roll_die(20)
-six_sided.roll_die(20)
-six_sided.roll_die(20)
-six_sided.roll_die(20)
-six_sided.roll_die(20)
+# makeing a 6 sided (in this case the default value ofthe attribute) and roll
+# it ten times
+six_sides = []
+while len(six_sides) != 10:
+    six = dice.Die() # created an instance of the Die class
+
+    six_side = six.roll_die()
+    six_sides.append(six_side)
+
+print(six_sides)
+
+# making a 10 sided-die and rolling it ten times
+ten_sideds = []
+while len(ten_sideds) != 10:
+    ten = dice.Die(10) # created an instance of the Die class
+    ten_sided = ten.roll_die()
+    ten_sideds.append(ten_sided)
+
+print(ten_sideds)
+
+# Making a 20-sided die and rolling it 10 times
+twenty_sideds = []
+while len(twenty_sideds) != 10:
+    twenty = dice.Die(20) # created an instance of the Die class
+    twenty_sided = twenty.roll_die()
+    twenty_sideds.append(twenty_sided)
+
+print(twenty_sideds)
